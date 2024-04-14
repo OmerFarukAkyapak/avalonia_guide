6d5b8cab3ae03f7b0ecedd6c464e5bdbf1259fca
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2024-04-14 14:52:06 +0300

    created avalonia app
    created avalonia mvvm application

---
+++ AvaloniaApp/AvaloniaGuideApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaGuideApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ AvaloniaApp/AvaloniaGuideApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaGuideApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
