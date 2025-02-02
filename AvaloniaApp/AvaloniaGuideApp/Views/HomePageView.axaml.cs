e69d2c2b7af60de05d7c4aff630fdac99c7601fc
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2025-02-02 18:50:59 +0300

    modifed project file structure and added navigation view to main window

---
+++ AvaloniaApp/AvaloniaGuideApp/Views/HomePageView.axaml.cs
@@ -0,0 +1,56 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaGuideApp;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+
+    private async void btnShowSplash_Click(object? sender, RoutedEventArgs e)
+    {
+        var splashScreen = new SplashScreenWindow();
+        splashScreen.Show();
+
+        await splashScreen.InitApp();
+        splashScreen.Close();
+    }
+
+    private void btnThemeSettings_Click(object? sender, RoutedEventArgs e)
+    {
+        var themeSettings = new ThemeSettingsWindow();
+        themeSettings.Show();
+    }
+
+    private async void btnTextInputDialog_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var owner =  VisualRoot as Window;
+        if (owner is null) 
+        {
+            return;
+        }
+
+        var result = await TextInputDialog.Prompt(
+            parentWindow: owner,
+            title: "Text Input Dialog Title",
+            caption: "Caption",
+            isRequired: true
+        );
+
+        if (result != null)
+        {
+            txtTextInputResult.Text = result;
+        }
+    }
+
+    private void btnShowConverterUsage_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var converterUsageWithDataGrid = new ConverterUsageWithDataGridWindow();
+        converterUsageWithDataGrid.Show();
+    }
+}
--- AvaloniaApp/AvaloniaGuideApp/Views/MainWindow.axaml.cs
+++ AvaloniaApp/AvaloniaGuideApp/Views/MainWindow.axaml.cs
@@ -1,49 +1,73 @@
+using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
+using AvaloniaGuideApp.Utils;
+using AvaloniaGuideApp.ViewModels;
+using FluentAvalonia.UI.Controls;
 using FluentAvalonia.UI.Windowing;
+using System;
 
 namespace AvaloniaGuideApp.Views
 {
     public partial class MainWindow : AppWindow
     {
+        private HomePageView _homePageView;
         public MainWindow()
         {
             InitializeComponent();
+
+            _homePageView = new HomePageView();
+            navigateView.Content = _homePageView;
         }
 
-        private async void btnShowSplash_Click(object? sender, RoutedEventArgs e)
+        private void Theme_Tapped(object? sender, Avalonia.Input.TappedEventArgs e)
         {
-            var splashScreen = new SplashScreenWindow();
-            splashScreen.Show();
-
-            await splashScreen.InitApp();
-            splashScreen.Close();
-        }
-
-        private void btnThemeSettings_Click(object? sender, RoutedEventArgs e)
-        {
-            var themeSettings = new ThemeSettingsWindow();
-            themeSettings.Show();
-        }
-
-        private async void btnTextInputDialog_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            var result = await TextInputDialog.Prompt(
-                parentWindow: this,
-                title: "Text Input Dialog Title",
-                caption: "Caption",
-                isRequired: true
-            );
-
-            if (result != null)
+            if (App.Current is null || App.Current.RequestedThemeVariant is null)
             {
-                txtTextInputResult.Text = result;
+                return;
+            }
+
+            var appTheme = App.Current.RequestedThemeVariant.ToString();
+
+            if (ThemeVariant.Dark.Key.ToString() == appTheme)
+            {
+                App.Current.RequestedThemeVariant = ThemeVariant.Light;
+                themeSymbol.Symbol = Symbol.WeatherSunny;
+
+            }
+            else if (ThemeVariant.Light.Key.ToString() == appTheme)
+            {
+                App.Current.RequestedThemeVariant = ThemeVariant.Dark;
+                themeSymbol.Symbol = Symbol.WeatherMoon;
             }
         }
 
-        private void btnShowConverterUsage_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void HomePage_Tapped(object? sender, Avalonia.Input.TappedEventArgs e)
         {
-            var converterUsageWithDataGrid = new ConverterUsageWithDataGridWindow();
-            converterUsageWithDataGrid.Show();
+            if (_homePageView is null)
+            {
+                _homePageView = new HomePageView();
+
+            }
+            navigateView.Content = _homePageView;
+        }
+
+        private async void Logout_Tapped(object? sender, Avalonia.Input.TappedEventArgs e)
+        {
+            var owner = VisualRoot as Window;
+            if (owner is null)
+            {
+                return;
+            }
+
+            var result = await TaskDialogHelper.ShowQuestionDialogAsync(owner, "Log Out", "Do you want to close the app ?");
+
+            if (result)
+            {
+                //Kill the app
+                await ProgressDialog.ShowProgressDialogWithDurationTime(owner, "Closing", "The application is closing ...", 3);
+                Environment.Exit(0);
+            }
         }
     }
 }
