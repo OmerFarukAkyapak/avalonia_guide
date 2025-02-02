b1e4714ef9bc9b7463e1a4f3587c841054d84137
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2025-02-02 18:49:26 +0300

    added progress dialog helper

---
+++ AvaloniaApp/AvaloniaGuideApp/Utils/ProgressDialog.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Windowing;
+using System.Threading.Tasks;
+using System;
+
+namespace AvaloniaGuideApp;
+
+public partial class ProgressDialog : AppWindow
+{
+    public ProgressDialog()
+    {
+        InitializeComponent();
+    }
+
+    public static async Task ShowProgressDialogWithDurationTime(Window owner, string title, string content, int durationInSeconds)
+    {
+        var progressDialog = new ProgressDialog();
+        progressDialog.TitleBar.Height = 0;
+        progressDialog.CanResize = false;
+        progressDialog.lblTitle.Content = title;
+        progressDialog.txtContent.Text = content;
+
+        progressDialog.ShowInTaskbar = false;
+
+        progressDialog.ShowDialog(owner);
+        await Task.Delay(TimeSpan.FromSeconds(durationInSeconds));
+        progressDialog.Close();
+    }
+
+    public static async Task<ProgressDialog> StartShowProgressDialog(Window owner, string title, string content)
+    {
+        var progressDialog = new ProgressDialog();
+        progressDialog.TitleBar.Height = 0;
+        progressDialog.CanResize = false;
+        progressDialog.lblTitle.Content = title;
+        progressDialog.txtContent.Text = content;
+
+        progressDialog.ShowInTaskbar = false;
+
+        progressDialog.ShowDialog(owner);
+        return progressDialog;
+    }
+
+    public static async Task CloseShowProgressDialog(ProgressDialog progressDialog)
+    {
+        progressDialog.Close();
+    }
+}
